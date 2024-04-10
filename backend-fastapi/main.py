1b8dab660643791d46f8d87a3da5ca9f2e3c1280
bhbghghbgb <hgbboyhuynhgiabao122003@yahoo.com>
2024-04-10 12:16:24 +0700

    fastapi: change to use beanie odm and default uvicorn

--- backend-fastapi/main.py
+++ backend-fastapi/main.py
@@ -1,8 +1,35 @@
+from contextlib import asynccontextmanager
 from typing import Union
 
+from beanie import Document, init_beanie
 from fastapi import FastAPI
+from motor.motor_asyncio import AsyncIOMotorClient
 
-app = FastAPI()
+
+class Account(Document):
+    username: str
+    password: str
+
+    class Settings:
+        name = "accounts"
+
+    class Config:
+        schema_extras = {"example": {"username": "memaybeo", "password": "123"}}
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # on startup
+    await init_beanie(database=database, document_models=[Account])
+    yield
+    # on shutdown
+
+
+app = FastAPI(lifespan=lifespan)
+client = AsyncIOMotorClient(
+    "mongodb+srv://new-user:KkuqGxB8oST1y7PB@cluster0.0tglxoe.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+)
+database = client.get_database("test")
 
 
 @app.get("/")
@@ -13,3 +40,8 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
+
+
+@app.get("/accounts")
+async def read_accounts() -> list[Account]:
+    return await Account.find_all().to_list()
