00a3f67cb5dffd23ee33139d8e74052f228499a8
sage-mint <hgbboyhuynhgiabao122003@yahoo.com>
2024-05-05 13:32:47 +0700

    implement backend apis

--- backend/app/models/accountModel.js
+++ backend/app/models/accountModel.js
@@ -1,11 +1,15 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const accountSchema = new mongoose.Schema({
     username: { type: String, required: true, trim: true },
     password: { type: String, required: true },
-    role: { type: String, required: true }
+    role: { type: String, required: true },
+    email: { type: String, required: false },
+    emailVerified: { type: String, default: false },
+    phone: { type: String, required: false },
+    phoneVerified: { type: String, default: false },
 });
 
-const Account = mongoose.model('Account', accountSchema);
+const Account = mongoose.model("Account", accountSchema);
 
 module.exports = Account;
