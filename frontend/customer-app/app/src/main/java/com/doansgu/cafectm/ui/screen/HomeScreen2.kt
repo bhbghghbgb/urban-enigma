71a6364dbbaa396ca479ba48b6de488a2e66dbe3
puenwyn <148056039+puenwyn@users.noreply.github.com>
2024-05-18 22:36:39 +0700

    Display the image on the cart screen

--- frontend/customer-app/app/src/main/java/com/doansgu/cafectm/ui/screen/HomeScreen2.kt
+++ frontend/customer-app/app/src/main/java/com/doansgu/cafectm/ui/screen/HomeScreen2.kt
@@ -36,7 +36,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
-import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
