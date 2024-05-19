33e37638b86c912c26eff6c86775ed7628207293
sage-mint <hgbboyhuynhgiabao122003@yahoo.com>
2024-05-20 01:51:25 +0700

    fix qr code scan khong ra (phai lam mau den va to hon)

--- frontend/customer-app/app/src/main/java/com/doansgu/cafectm/ui/screen/HomeScreen2.kt
+++ frontend/customer-app/app/src/main/java/com/doansgu/cafectm/ui/screen/HomeScreen2.kt
@@ -1,6 +1,5 @@
 package com.doansgu.cafectm.ui.screen
 
-import android.util.Log
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
