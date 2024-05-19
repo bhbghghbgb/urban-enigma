15ce3bd9591b49b4f0e7835dce7e631aa3f36427
puenwyn <148056039+puenwyn@users.noreply.github.com>
2024-05-19 22:48:44 +0700

    feat: delivery confirmation

--- frontend/shipper-app/app/src/main/java/com/example/delivery_app/repository/OrderRepository.kt
+++ frontend/shipper-app/app/src/main/java/com/example/delivery_app/repository/OrderRepository.kt
@@ -1,12 +1,20 @@
 package com.example.delivery_app.repository
 
 import com.example.delivery_app.model.Order
+import com.example.delivery_app.model.ResponseFromServer
 import com.example.delivery_app.model.RetrofitInstance
+import com.example.delivery_app.model.Status
 import retrofit2.Response
 
 class OrderRepository {
     private val orderService = RetrofitInstance.apiService
     suspend fun getOrderById(id: String): Response<Order> {
         return orderService.getOrderById(id)
+    }
+    suspend fun updateStatusDelivery(id: String): Response<ResponseFromServer> {
+        return orderService.updateStatusDelivery(id)
+    }
+    suspend fun updateStatusOrder(id: String, status: Status): Response<ResponseFromServer> {
+        return orderService.updateStatusOrder(id, status)
     }
 }
